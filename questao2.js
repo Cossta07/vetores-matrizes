1f17b3f2ca4f554d98bbbe7c2e0790b8d169f1d1
Cossta07 <samuellinker46@gmail.com>
2024-04-03 15:47:03 -0400

    questao 2 feita

---
+++ questao2.js
@@ -0,0 +1,20 @@
+/*
+Crie um código em JavaScript para adicionar um elemento no final de
+um vetor.
+*/
+
+let meuarray = [];
+
+
+meuarray.push(1);
+meuarray.push(2);
+meuarray.push(3);
+meuarray.push(4);
+meuarray.push(5);
+
+
+console.log(meuarray);
+
+meuarray.push(6);
+
+console.log(meuarray);
