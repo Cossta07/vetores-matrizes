30d38f8a7ab36a4139a2f5018598c05f8d7ed17f
Cossta07 <samuellinker46@gmail.com>
2024-04-03 16:39:33 -0400

    questao 6 feita

--- questao6.js
+++ questao6.js
@@ -2,3 +2,19 @@
 Escreva um código para calcular a soma de todos os elementos de uma
 matriz em JavaScript.
 */
+
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+let soma = 0;
+
+for(let i = 0; i < matriz.length;i++){
+    for(let j=0;j < matriz[i].length;j++){
+    soma += (matriz[i][j]);
+    }
+
+}
+console.log(soma);
